51ce890abf6297c7dc3f4fcc58c3cad103e70b50
Ratchet <klaus@sentinel.dyndns.info>
2011-04-26 21:45:57 +0200

    changed setup.py

--- setup.py
+++ setup.py
@@ -1,8 +1,5 @@
-from distutils.core import setup
+from setuptools import setup
 
-#This is a list of files to install, and where
-#(relative to the 'root' dir, where setup.py is)
-#You could be more specific.
 files = ['images/*']
 
 setup(name = "warren",
@@ -11,19 +8,8 @@ setup(name = "warren",
     url="http://example.com",
     author = "Ratchet",
     author_email = "yeah@sure.net",
-    #Name the folder where your packages live:
-    #(If you have other packages (dirs) or modules (py files) then
-    #put them into the package directory - they will be found 
-    #recursively.)
     packages = ['warren','warren.ui','warren.core'],
-    #'package' package must contain files (see list above)
-    #I called the package 'package' thus cleverly confusing the whole issue...
-    #This dict maps the package name =to=> directories
-    #It says, package *needs* these files.
     package_data = {'warren' : files },
-    #'runner' is in the root.
     scripts = ["Warren"],
-    #
-    #This next part it for the Cheese Shop, look a little down the page.
-    #classifiers = []     
+    install_requires = ['configobj>=4.7.2'],
 ) 
