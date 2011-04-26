72a6d2df1386621369aae0befcca3acc2ebdcee4
Ratchet <klaus@sentinel.dyndns.info>
2011-04-26 21:17:29 +0200

    make warren.py work with new dir structure

--- warren.py
+++ warren.py
@@ -1,8 +1,10 @@
+#!/usr/bin/env python2
+
 from PyQt4.QtGui import QApplication
-from ui.MainWindow import MainWindow
+from warren.ui.MainWindow import MainWindow
 import sys
 
-if __name__ == '__main__':
+def start():
     app = QApplication(sys.argv)
     app.setApplicationName('Warren')
     app.setQuitOnLastWindowClosed(True)
@@ -11,3 +13,6 @@ if __name__ == '__main__':
     window.show()
 
     sys.exit(app.exec_())
+
+if __name__ == '__main__':
+    start()
