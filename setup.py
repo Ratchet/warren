69509e17aa6c17aaf9535342d8ee2530e4b1459d
Ratchet <klaus@sentinel.dyndns.info>
2011-05-01 17:34:05 +0200

    raise version number in setup.py

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@ from setuptools import setup
 files = ['images/*']
 
 setup(name = "warren",
-    version = "0.2",
+    version = "0.2.1",
     description = "A dropzone application for Freenet",
     url="http://example.com",
     author = "Ratchet",
-    author_email = "yeah@sure.net",
+    author_email = "klaus@sentinel.dyndns.info",
     packages = ['warren','warren.ui','warren.core'],
     package_data = {'warren' : files },
     scripts = ["Warren"],
