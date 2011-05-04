2b07fdc982ccea7d5db79be5415b7d2784757981
Ratchet <klaus@sentinel.dyndns.info>
2011-05-04 21:43:40 +0200

    make pastebin resizable

--- warren/ui/PastebinDialog.py
+++ warren/ui/PastebinDialog.py
@@ -1,8 +1,8 @@
 # -*- coding: utf-8 -*-
 
-# Form implementation generated from reading ui file 'PastebinDialog.ui'
+# Form implementation generated from reading ui file 'warren/ui/PastebinDialog.ui'
 #
-# Created: Sun Apr 24 14:57:22 2011
+# Created: Wed May  4 21:42:41 2011
 #      by: PyQt4 UI code generator 4.7.3
 #
 # WARNING! All changes made in this file will be lost!
@@ -19,24 +19,26 @@ class Ui_PastebinDialog(object):
         sizePolicy.setHeightForWidth(PastebinDialog.sizePolicy().hasHeightForWidth())
         PastebinDialog.setSizePolicy(sizePolicy)
         PastebinDialog.setMinimumSize(QtCore.QSize(500, 400))
-        PastebinDialog.setMaximumSize(QtCore.QSize(500, 400))
+        PastebinDialog.setMaximumSize(QtCore.QSize(16777215, 16777215))
         PastebinDialog.setAcceptDrops(False)
+        self.verticalLayout = QtGui.QVBoxLayout(PastebinDialog)
+        self.verticalLayout.setObjectName("verticalLayout")
+        self.label = QtGui.QLabel(PastebinDialog)
+        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.label)
         self.plainTextEdit = QtGui.QPlainTextEdit(PastebinDialog)
-        self.plainTextEdit.setGeometry(QtCore.QRect(5, 5, 490, 345))
         self.plainTextEdit.setLineWrapMode(QtGui.QPlainTextEdit.NoWrap)
         self.plainTextEdit.setObjectName("plainTextEdit")
-        self.label = QtGui.QLabel(PastebinDialog)
-        self.label.setGeometry(QtCore.QRect(10, 360, 271, 25))
-        self.label.setObjectName("label")
+        self.verticalLayout.addWidget(self.plainTextEdit)
         self.buttonBox = QtGui.QDialogButtonBox(PastebinDialog)
-        self.buttonBox.setGeometry(QtCore.QRect(320, 360, 173, 25))
         self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.Ok)
         self.buttonBox.setObjectName("buttonBox")
+        self.verticalLayout.addWidget(self.buttonBox)
 
         self.retranslateUi(PastebinDialog)
         QtCore.QMetaObject.connectSlotsByName(PastebinDialog)
 
     def retranslateUi(self, PastebinDialog):
         PastebinDialog.setWindowTitle(QtGui.QApplication.translate("PastebinDialog", "Pastebin", None, QtGui.QApplication.UnicodeUTF8))
-        self.label.setText(QtGui.QApplication.translate("PastebinDialog", "Enter text to insert above", None, QtGui.QApplication.UnicodeUTF8))
+        self.label.setText(QtGui.QApplication.translate("PastebinDialog", "Enter text to insert below", None, QtGui.QApplication.UnicodeUTF8))
 
