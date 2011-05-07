091242383a88d836318713b83623394c83343561
Ratchet <klaus@sentinel.dyndns.info>
2011-05-07 10:41:21 +0200

    rename the starter script because of windows, grrr

--- setup.py
+++ setup.py
@@ -3,13 +3,13 @@ from setuptools import setup
 files = ['images/*']
 
 setup(name = "warren",
-    version = "0.2.1",
+    version = "0.2.2",
     description = "A dropzone application for Freenet",
     url="http://example.com",
     author = "Ratchet",
     author_email = "klaus@sentinel.dyndns.info",
     packages = ['warren','warren.ui','warren.core'],
     package_data = {'warren' : files },
-    scripts = ["Warren"],
+    scripts = ["WarrenUI"],
     install_requires = ['configobj>=4.7.2'],
 ) 
